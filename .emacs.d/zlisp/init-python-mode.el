b5b2b07044e6c1949c5d0fe4c1f0a330fbb1bd80
zhousz <zhousz@fiberhome>
2017-10-31 13:47:15 +0800

    fucked python-mode up. next to c/cpp

--- .emacs.d/zlisp/init-python-mode.el
+++ .emacs.d/zlisp/init-python-mode.el
@@ -6,13 +6,22 @@
 (require-package 'pip-requirements)
 
 (when (maybe-require-package 'anaconda-mode)
+  (require 'f)
+  (defcustom anaconda-mode-installation-directory
+    (f-parent (locate-library "anaconda-mode"))
+    "Installation directory for anaconda-mode server."
+    :group 'anaconda-mode
+    :type 'directory)
   (after-load 'python
     (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+    ;set interpreter to ipython, as everybook recommended
+    (setq python-shell-interpreter "ipython"
+	  python-shell-interpreter-args "-i"))
   (when (maybe-require-package 'company-anaconda)
     (after-load 'company
       (add-hook 'python-mode-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
+                (lambda () (sanityinc/local-push-company-backend '(company-anaconda :with company-yasnippet)))))))
 
 
 (provide 'init-python-mode)
